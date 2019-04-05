a17584c39c5351c9addcfb5be3a3d8f44739c3fe
Guillem Pablo Joan <joan.vinyals@est.fib.upc.edu>
2019-04-05 07:59:27 +0000

    casi funciona

--- lab3/main.c
+++ lab3/main.c
@@ -14,7 +14,7 @@ void * gpio_map;
 volatile unsigned *gpio;
 
 #define GPIO_setValue(pin_num, value)\
-	*(gpio+7+(-value&3)) = 1 << pin_num
+	*(gpio+7+(-(!value)&3)) = 1 << pin_num
 
 #define GPIO_readValue(pin_num)\
 	*(gpio+13)&=1<<pin_num
@@ -28,12 +28,15 @@ volatile unsigned *gpio;
 	*(gpio+(pin_num/10)) &= ~(7<<((pin_num%10)*3))
 
 
-#define CLK_PERIOD 1<<15
-#define CLK  26
-#define Dout 19
-#define Din  13
-#define CS   6
+#define CLK_PERIOD 1<<21
+#define CLK  11
+#define Dout 9
+#define Din  10
+#define CS   8
 
+enum state_t {CS_UP, CS_DOWN, SB, CONF, D2, D1, D0, W1, W2, READ, IDLE};
+
+static enum state_t STATE;
 int GPIO_init(){
 
 	if((mem_fd = open("/dev/mem", O_RDWR|O_SYNC))<0){
@@ -52,20 +55,64 @@ int GPIO_init(){
 }
 
 int SpiRead() {
-	
+	int read = 0;	
 	int contador = 0;
 	int clk_value = 0;	
+	int read_count = 0;
 
-	GPIO_setValue( CS, 1 );
-	for (int i = 0; i < CLK_PERIOD; i++);
-	GPIO_setValue( CS, 0 );
 	while (1) {	
 		if(contador>=CLK_PERIOD){
 			clk_value ^= 1;
 			GPIO_setValue( CLK, clk_value );
-			if (clk_value) {
+			if (!clk_value) {
+				switch( STATE ){
+					case READ:
+						if ( read_count++ >= 12){ 
+							STATE=CS_UP; 
+							read_count = 0;
+							printf("Value read: %d\n", read); 
+							read = 0;
+						}
+						break;
+					default:
+						STATE = (STATE + 1);	
+						break;
+				}	
+			//	printf("State %d\n", STATE);
 
 			} else {
+				switch( STATE ){
+					case CS_UP:
+						GPIO_setValue( CS, 1);
+						break;
+					case CS_DOWN:
+						GPIO_setValue( CS, 0);
+						break;
+					case SB:
+						GPIO_setValue( Din,1);
+						break;
+					case CONF:
+						GPIO_setValue( Din,1); 	
+						break;
+					case D2:
+						GPIO_setValue( Din,0);
+						break;
+					case D1:
+						GPIO_setValue( Din,0);
+						break;
+					case D0:
+						GPIO_setValue( Din,0);
+						break;
+					case READ:
+						GPIO_setValue( Din,0);
+						read <<= 1;
+						int tmp = ( GPIO_readValue( Dout ) );
+						printf("Pin read %d\n", tmp); 
+						read  |= tmp; //( GPIO_readValue( Dout ));
+						break;
+					default:
+						break;
+				}
 
 			}
 			contador = 0;
